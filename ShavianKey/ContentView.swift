ca936c670b9aac2e9552eccc4e9f69ede5754d57
M. Elijah Wangeman <maw4678@rit.edu>
2025-10-22 05:21:58 -0400

    Initial Commit

---
+++ ShavianKey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShavianKey
+//
+//  Created by koteczek on 10/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShavianKey/ShavianKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShavianKeyApp.swift
+//  ShavianKey
+//
+//  Created by koteczek on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ShavianKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
